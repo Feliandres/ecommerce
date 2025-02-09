4bfdd463e321fde2e86080a1e5327897e0fe34a8
Feliandres <85601974+Feliandres@users.noreply.github.com>
2025-02-08 20:48:53 -0500

    02-create-website-layout

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = 
+process.env.NEXT_PUBLIC_APP_NAME || "Ecommerce"
+export const APP_SLOGAN =
+process.env.NEXT_PUBLIC_APP_SLOGAN || "Your slogan here"
+export const APP_DESCRIPTION =
+process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Website create with Next.js and MongoDB"
