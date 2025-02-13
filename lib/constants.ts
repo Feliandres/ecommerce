ef35d78e2e79c20da949c23d8a4a4da7aa1a8bfe
Feliandres <85601974+Feliandres@users.noreply.github.com>
2025-02-13 15:15:40 -0500

    09-create-product-detail-page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,7 @@
-export const APP_NAME = 
+export const APP_NAME =
 process.env.NEXT_PUBLIC_APP_NAME || "Ecommerce"
 export const APP_SLOGAN =
 process.env.NEXT_PUBLIC_APP_SLOGAN || "Your slogan here"
 export const APP_DESCRIPTION =
 process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Website create with Next.js and MongoDB"
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
