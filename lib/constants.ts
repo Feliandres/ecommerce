2394fb12fdd8b52379a4833b32fbfa0ca38b9381
Feliandres <85601974+Feliandres@users.noreply.github.com>
2025-02-16 22:04:50 -0500

    11-implement-add-to-cart

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,10 @@
 export const APP_NAME =
-process.env.NEXT_PUBLIC_APP_NAME || "Ecommerce"
+    process.env.NEXT_PUBLIC_APP_NAME || "Ecommerce"
 export const APP_SLOGAN =
-process.env.NEXT_PUBLIC_APP_SLOGAN || "Your slogan here"
+    process.env.NEXT_PUBLIC_APP_SLOGAN || "Your slogan here"
 export const APP_DESCRIPTION =
-process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Website create with Next.js and MongoDB"
-export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
+    process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Website create with Next.js and MongoDB"
+export const PAGE_SIZE = 
+    Number(process.env.PAGE_SIZE || 9)
+export const FREE_SHIPPING_MIN_PRICE = Number(
+    process.env.FREE_SHIPPING_MIN_PRICE || 35)
