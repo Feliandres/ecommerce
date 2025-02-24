34b85c8a5d45abd78b043bd98e3d8e5966b386e5
Feliandres <85601974+Feliandres@users.noreply.github.com>
2025-02-23 22:35:58 -0500

    17-create-checkout-page

--- lib/validator.ts
+++ lib/validator.ts
@@ -73,19 +73,6 @@ export const OrderItemSchema = z.object({
     size: z.string().optional(),
     color: z.string().optional(),
 })
-// Cart
-export const CartSchema = z.object({
-    items: z
-        .array(OrderItemSchema)
-        .min(1, 'Order must contain at least one item'),
-    itemsPrice: z.number(),
-    taxPrice: z.optional(z.number()),
-    shippingPrice: z.optional(z.number()),
-    totalPrice: z.number(),
-    paymentMethod: z.optional(z.string()),
-    deliveryDateIndex: z.optional(z.number()),
-    expectedDeliveryDate: z.optional(z.date()),
-})
 // UserInput
 export const UserInputSchema = z.object({
     name: UserName,
@@ -117,4 +104,28 @@ export const UserSignUpSchema = UserSignInSchema.extend({
 }).refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
     path: ['confirmPassword'],
+})
+// ShippingAddress
+export const ShippingAddressSchema = z.object({
+    fullName: z.string().min(1, 'Full name is required'),
+    street: z.string().min(1, 'Address is required'),
+    city: z.string().min(1, 'City is required'),
+    postalCode: z.string().min(1, 'Postal code is required'),
+    province: z.string().min(1, 'Province is required'),
+    phone: z.string().min(1, 'Phone number is required'),
+    country: z.string().min(1, 'Country is required'),
+})
+// Cart
+export const CartSchema = z.object({
+    items: z
+        .array(OrderItemSchema)
+        .min(1, 'Order must contain at least one item'),
+    itemsPrice: z.number(),
+    taxPrice: z.optional(z.number()),
+    shippingPrice: z.optional(z.number()),
+    totalPrice: z.number(),
+    paymentMethod: z.optional(z.string()),
+    shippingAddress: z.optional(ShippingAddressSchema),
+    deliveryDateIndex: z.optional(z.number()),
+    expectedDeliveryDate: z.optional(z.date()),
 })
