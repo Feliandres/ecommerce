ea518e392cbf0806b40756d807d7312e9f433844
Feliandres <85601974+Feliandres@users.noreply.github.com>
2025-02-26 15:44:48 -0500

    25-create-search-page

--- lib/utils.ts
+++ lib/utils.ts
@@ -23,7 +23,6 @@ export function formUrlQuery({
     { skipNull: true }
   )
 }
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
@@ -122,7 +121,6 @@ export function timeUntilMidnight(): { hours: number; minutes: number } {
 
     return { hours, minutes }
 }
-
 export const formatDateTime = (dateString: Date) => {
     const dateTimeOptions: Intl.DateTimeFormatOptions = {
       month: 'short', // abbreviated month name (e.g., 'Oct')
@@ -163,4 +161,38 @@ export const formatDateTime = (dateString: Date) => {
 }
 export function formatId(id: string) {
     return `..${id.substring(id.length - 6)}`
+}
+export const getFilterUrl = ({
+  params,
+  category,
+  tag,
+  sort,
+  price,
+  rating,
+  page,
+}: {
+  params: {
+    q?: string
+    category?: string
+    tag?: string
+    price?: string
+    rating?: string
+    sort?: string
+    page?: string
+  }
+  tag?: string
+  category?: string
+  sort?: string
+  price?: string
+  rating?: string
+  page?: string
+}) => {
+  const newParams = { ...params }
+  if (category) newParams.category = category
+  if (tag) newParams.tag = toSlug(tag)
+  if (price) newParams.price = price
+  if (rating) newParams.rating = rating
+  if (page) newParams.page = page
+  if (sort) newParams.sort = sort
+  return `/search?${new URLSearchParams(newParams).toString()}`
 }
